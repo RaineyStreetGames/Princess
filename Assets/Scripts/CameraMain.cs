dea82628a85aeae3de63b4a9c227b9c5bb53e321
Jesse Michael <jesse.michael@faithlife.com>
2019-06-17 23:08:38 -0700

    Hero! is moving!

---
+++ Assets/Scripts/CameraMain.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMain : MonoBehaviour
+{
+    public GameObject player;
+    public float cameraHeight = 7.0f;
+    public float cameraDepth = 14.0f;
+
+    void Update()
+    {
+        Vector3 pos = player.transform.position;
+        pos.y += cameraHeight;
+        pos.z -= cameraDepth;
+        transform.position = pos;
+    }
+}
---
+++ Assets/Scripts/Hero.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hero : MonoBehaviour
+{
+
+    public float speed = 5.0f;
+    // public Animator animator;
+
+    private Animator animator;
+    private Vector3? targetPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (targetPosition.HasValue)
+        {
+
+            // Debug.Log("has value");
+            // rotate towards the targetPosition
+
+
+            // move towards the targetPosition
+            transform.position = Vector3.MoveTowards(transform.position, targetPosition.Value, speed * Time.deltaTime);
+
+            if (transform.localPosition == targetPosition)
+            {
+                targetPosition = null;
+                animator.SetTrigger("Stop");
+            }
+        }
+
+        // Mouse Drag Event
+        // if (Input.GetMouseButton(0)) {
+        // }
+
+        // Mouse Up Event
+        if (Input.GetMouseButtonUp(0))
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit) && hit.transform.tag == "Terrain")
+            {
+                targetPosition = hit.point;
+                transform.forward = hit.point - transform.position;
+                animator.SetTrigger("Run");
+            }
+            Debug.Log($"Clicked {hit.transform.name} @ {hit.point}");
+        }
+    }
+}
