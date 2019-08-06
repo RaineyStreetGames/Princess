0315b321995a2d6dfd59214a7fadc2090fd0fbc8
Jesse Michael <jesse.michael@faithlife.com>
2019-08-05 22:44:54 -0700

    Dungeon entry

--- Assets/Scripts/CameraMain.cs
+++ Assets/Scripts/CameraMain.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class CameraMain : MonoBehaviour
 {
-    public GameObject player;
-    public float cameraHeight = 7.0f;
-    public float cameraDepth = 14.0f;
+    public GameObject target;
+    public Vector3 offset;
 
-    void Update()
+    void LateUpdate()
     {
-        Vector3 pos = player.transform.position;
-        pos.y += cameraHeight;
-        pos.z -= cameraDepth;
-        transform.position = pos;
+        transform.position = target.transform.position + offset;
+        transform.LookAt(target.transform);
     }
 }
