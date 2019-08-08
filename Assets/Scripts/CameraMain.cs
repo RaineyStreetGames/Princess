e1adc786390e24db4282844d93477731b522b1c0
Jesse Michael <jesse.michael@faithlife.com>
2019-08-07 22:08:04 -0700

    Lighting, Baking Nave Mesh, Camera work.

--- Assets/Scripts/CameraMain.cs
+++ Assets/Scripts/CameraMain.cs
@@ -6,10 +6,23 @@ public class CameraMain : MonoBehaviour
 {
     public GameObject target;
     public Vector3 offset;
+    public float up;
+    public float speed = 1.0f;
+    public float angle = 20.0f;
 
     void LateUpdate()
     {
         transform.position = target.transform.position + offset;
-        transform.LookAt(target.transform);
+        transform.LookAt(new Vector3(target.transform.position.x, target.transform.position.y + up, target.transform.position.z));
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            offset = Quaternion.AngleAxis(90.0f * Time.deltaTime, Vector3.up) * offset;
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            offset = Quaternion.AngleAxis(-90.0f * Time.deltaTime, Vector3.up) * offset;
+        }
     }
 }
